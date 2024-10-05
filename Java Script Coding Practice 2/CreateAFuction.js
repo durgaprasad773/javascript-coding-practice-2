79668c3ea97f13955aacaf19d0dcb53e05e7e704
durgaprasa773 <pdurgaprasad0707@gmail.com>
2024-10-05 11:14:56 +0530

    JavaScript Coding Practice 2

---
+++ Java Script Coding Practice 2/CreateAFuction.js
@@ -0,0 +1,10 @@
+/*
+ * Write your code here and return the output.
+ */
+function getNationalBird(){
+    return 'Peacock'
+}
+ 
+
+let nationalBird = getNationalBird();
+console.log(nationalBird);
